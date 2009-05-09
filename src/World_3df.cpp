4cbda8e82ac03ca186d19b7275414288f3686954
Athantor <athantor@gmail.com>
2009-05-09 16:39:44 +0200

    The real deal world in which values for function are going to be sought

---
+++ src/World_3df.cpp
@@ -0,0 +1,17 @@
+/*
+ * World_3df.cpp
+ *
+ *  Created on: 2009-05-09
+ *      Author: Krzys
+ */
+
+#include "World_3df.h"
+
+World_3df::World_3df() {
+	// TODO Auto-generated constructor stub
+
+}
+
+World_3df::~World_3df() {
+	// TODO Auto-generated destructor stub
+}
---
+++ src/World_3df.h
@@ -0,0 +1,19 @@
+/*
+ * World_3df.h
+ *
+ *  Created on: 2009-05-09
+ *      Author: Krzys
+ */
+
+#ifndef WORLD_3DF_H_
+#define WORLD_3DF_H_
+
+#include "World.h"
+
+class World_3df: public World {
+public:
+	World_3df();
+	virtual ~World_3df();
+};
+
+#endif /* WORLD_3DF_H_ */
